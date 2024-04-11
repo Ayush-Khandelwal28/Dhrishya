3cdb7c382e28e33652e89955e7aa80997b248c9a
Ayush Khandelwal <ayushkhandelwalak28.com>
2024-04-11 19:55:33 +0530

    Added Auth using Clerk

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import MobileNav from './MobileNav'
+import { SignedIn, UserButton } from '@clerk/nextjs'
 
 const Navbar = () => {
   return (
@@ -17,8 +18,10 @@ const Navbar = () => {
         <p className='text-white text-[26px] font-extrabold max-sm:hidden'>Dhrishya</p>
       </Link>
 
-      <div className='flex-between gap-5'>
-        {/* User Management */}
+      <div className='flex-between gap-5 text-white'>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
         <MobileNav />
       </div>
 
---
+++ middleware.ts
@@ -0,0 +1,11 @@
+import { authMiddleware } from "@clerk/nextjs";
+
+const protectedRoutes = (["/", "/previous", "/upcoming", "/recordings", "/personalRoom", "/meeting(.*)"]);
+
+export default authMiddleware({
+    publicRoutes: ["/"]
+});
+
+export const config = {
+    matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
